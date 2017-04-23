f70c8b0ea243d00adfce87c05ec1b85a8824bbd0
Apisitsakul <apisitsakul@gmail.com>
2017-04-23 17:16:46 +0700

    add index.php 4

--- index.php
+++ index.php
@@ -19,8 +19,16 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => "i don't understand"
 			];
+
+			if( $text == 'Hi'){
+				$messages = [
+				'type' => 'text',
+				'text' => "Hello"
+			];
+			
+			}
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
